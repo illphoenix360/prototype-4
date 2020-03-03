aa259fc7db25c2ff1a037f7e5cff20f27096e037
illphoenix360 <56093731+illphoenix360@users.noreply.github.com>
2020-03-03 08:45:03 -0800

    i made an enemy made the enemy follow the player and cleande up a lot of coding also i got the camera working correctly.

---
+++ Assets/scrips/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+   private Rigidbody playerRb;
+   public float speed = 5.0f;
+   private GameObject focalpoint;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+        focalpoint = GameObject.Find("Focal Point");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      float forwardInput = Input.GetAxis("Vertical");
+     playerRb.AddForce(focalpoint.transform.forward * forwardInput * speed);
+    }
+
+}
